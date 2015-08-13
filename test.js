938812c48836be74c4fa2c3ac22b0755f5eb0601
Pat Cavit <pcavit@gmail.com>
2015-08-12 17:14:11 -0700

    First simple working version
    Has some graphical bugs though!

--- test.js
+++ test.js
@@ -4,16 +4,39 @@ var fs  = require("fs"),
     PNG = require("png-stream/decoder"),
     JPG = require("jpg-stream/encoder"),
     Color = require("color-transform"),
-    Alpha = require("./lib/alpha-extractor");
+    Alpha = require("./lib/alpha-extractor"),
+    template = require("lodash.template"),
+    ASQ = require("asynquence"),
+    concat = require("concat-stream");
 
-fs.createReadStream("./alert.png")
-    .pipe(new PNG())
-    .pipe(new Alpha())
-    .pipe(new JPG())
-    .pipe(fs.createWriteStream('./mask.jpg'));
+ASQ()
+    .gate(
+        function(done) {
+            fs.createReadStream("./alert.png")
+                .pipe(new PNG())
+                .pipe(new Alpha())
+                .pipe(new JPG())
+                .pipe(concat(done));
+        },
+        function(done) {
+            fs.createReadStream("./alert.png")
+                .pipe(new PNG())
+                .pipe(new Color("rgb"))
+                .pipe(new JPG())
+                .pipe(concat(done));
+        }
+    )
+    .then(function(done, mask, image) {
+        // TODO: determine image dimensions
+        var svg = template(fs.readFileSync("./lib/svg-template.xml", "utf8"))({
+                width  : 16,
+                height : 16,
+                image  : image.toString("base64"),
+                mask   : mask.toString("base64")
+            });
 
-fs.createReadStream("./alert.png")
-    .pipe(new PNG())
-    .pipe(new Color("rgb"))
-    .pipe(new JPG())
-    .pipe(fs.createWriteStream('./contents.jpg'));
+        console.log(svg); // TODO: REMOVE DEBUGGING
+
+        fs.writeFileSync("./output.svg", svg);
+    });
+
